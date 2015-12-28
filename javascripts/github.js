6062052ee82cfd7de5b70b470f1b297173dd143a
Raphaël Pinson <raphink@gmail.com>
2015-12-28 13:25:46 +0100

    attr()

--- javascripts/github.js
+++ javascripts/github.js
@@ -1,5 +1,5 @@
 function getLatestReleaseInfo(elem) {
-        var project = elem.getAttribute("data-repo");
+        var project = elem.attr("data-repo");
 
         $.getJSON("https://api.github.com/repos/cc-translators/"+project+"/releases/latest").done(function (release) {
             console.log(release);
