84da6ab45ce9f58de74ca3eff36e94c4992483d5
Raphaël Pinson <raphink@gmail.com>
2015-12-28 13:19:06 +0100

    Add github.js

---
+++ javascripts/github.js
@@ -0,0 +1,14 @@
+function getLatestReleaseInfo(elem) {
+        var project = elem.getAttribute("data-repo");
+
+        $.getJSON("https://api.github.com/repos/cc-translators/"+project+"/releases/latest").done(function (release) {
+            console.log(release);
+        });
+    }
+
+
+function getAllReleaseInfos() {
+    $(".latest-release").each(function (e) {
+        getLatestReleaseInfo(e);
+    });
+}
